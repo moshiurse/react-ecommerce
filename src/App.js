5699af1f7f2f3f7653f55f2e8da75bb102d328fc
Moshiur Rahman <rmoshiur705@gmail.com>
2019-12-11 00:00:42 +0600

    Routing with withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import {Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <h1>Hats Page</h1>
+)
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+    <Switch>
+      <Route exact path='/' component={HomePage}/>
+      <Route path='/hats' component={HatsPage}/>
+    </Switch>
+      
     </div>
   );
 }
