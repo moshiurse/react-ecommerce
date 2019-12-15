3a9fa04579b35bc7a9534c506646e0f28b4ee4ff
Moshiur Rahman <rmoshiur705@gmail.com>
2019-12-15 21:09:38 +0600

    create SignUp Component and sign up with email pass and saved to firestore

--- src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './sign-in-sign-up.styles.scss';
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 const SigninAndSignUpPage = () => (
     <div className="signin">
         <SignIn />
+        <SignUp />
     </div>
 );
 
