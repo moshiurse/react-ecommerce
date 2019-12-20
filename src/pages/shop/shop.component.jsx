e719a7a86d47640ab6e638060b7c1b93a8ec5fdb
Moshiur Rahman <rmoshiur705@gmail.com>
2019-12-20 09:17:05 +0600

    Add Direcory & shop to root reducers

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
+
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import CollectionPreview from '../../components/collection-preview/collection-preview';
 
-class ShopPage extends React.Component{
-    constructor(props){
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const { collections } = this.state;
-        return (
+import { selectCollections } from '../../redux/shop/shop.selectors';
+const ShopPage = ({ collections }) => (
           <div className='shop-page'>
             {
                 collections.map(({ id, ...otherCollectionProps }) => (
@@ -22,8 +14,10 @@ class ShopPage extends React.Component{
             ))
             }
           </div>
-        );
-      }
-    }
-    
-    export default ShopPage;
+        );  
+
+      const mapStateToProps = createStructuredSelector({
+        collections: selectCollections
+      })
+
+    export default connect(mapStateToProps)(ShopPage);
